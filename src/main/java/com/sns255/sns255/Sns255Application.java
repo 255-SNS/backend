868d243a309c98dab0e471ca190aa7254f98e1a7
silkair <silkair1234@gmail.com>
2024-11-08 18:01:49 +0900

    Initial Setup

---
+++ src/main/java/com/sns255/sns255/Sns255Application.java
@@ -0,0 +1,13 @@
+package com.sns255.sns255;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sns255Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sns255Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sns255/sns255/Sns255ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns255.sns255;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sns255ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
