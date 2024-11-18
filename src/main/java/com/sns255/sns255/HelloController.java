60ca9229eacca15ab1be3ca1367b61f78a3a79bc
silkair <silkair1234@gmail.com>
2024-11-19 01:13:03 +0900

    feat(swagger): global swaggerconfig 문서 파일 작성

--- src/main/java/com/sns255/sns255/HelloController.java
+++ src/main/java/com/sns255/sns255/HelloController.java
@@ -1,10 +1,11 @@
 package com.sns255.sns255;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 public class HelloController {
-
     @GetMapping("/")
     public String sayHello() {
         return "Hello Java";
