d14792dddd0629df9b339baddac523f054d23d97
silkair <silkair1234@gmail.com>
2024-11-19 16:33:08 +0900

    feat(entity): create Post table

---
+++ src/main/java/com/sns255/sns255/domain/post/entity/Post.java
@@ -0,0 +1,52 @@
+package com.sns255.sns255.domain.post.entity;
+
+import com.sns255.sns255.domain.board.entity.Board;
+import com.sns255.sns255.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Boolean isDeleted = false;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    protected Post() {
+    }
+}
