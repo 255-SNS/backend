0d514aab47c3ad52bf9c2e2ac77fbf7f320d4ed6
silkair <silkair1234@gmail.com>
2024-11-26 17:34:11 +0900

    refactor(team) : 랜덤 팀배정 구현 (인원수 맞춰서)

--- src/main/java/com/sns255/sns255/domain/user/entity/Team.java
+++ src/main/java/com/sns255/sns255/domain/user/entity/Team.java
@@ -3,5 +3,15 @@ package com.sns255.sns255.domain.user.entity;
 public enum Team {
     BLUE,
     GREEN,
-    RED
+    RED;
+
+    public static Team getBalancedTeam(long blueCount, long greenCount, long redCount) {
+        if (blueCount <= greenCount && blueCount <= redCount) {
+            return Team.BLUE;
+        } else if (greenCount <= blueCount && greenCount <= redCount) {
+            return Team.GREEN;
+        } else {
+            return Team.RED;
+        }
+    }
 }
