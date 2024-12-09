9b1beb74d1222a64103b6b3e656fa0520e7eecc5
silkair <silkair1234@gmail.com>
2024-12-09 16:10:59 +0900

    fix(repo) : UserRepository 이메일로 검색 추가

--- src/main/java/com/sns255/sns255/domain/user/repository/UserRepository.java
+++ src/main/java/com/sns255/sns255/domain/user/repository/UserRepository.java
@@ -6,11 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // 이메일로 사용자 검색
+    Optional<User> findByEmail(String email);
+    // 이메일 중복 확인
     boolean existsByEmail(String email);
+    // 학번 중복 확인
     boolean existsByStudentId(int studentId);
-
+    // 팀별 사용자 수 계산
     @Query("SELECT COUNT(u) FROM User u WHERE u.team = :team")
     long countByTeam(Team team);
 }
