7df4d7dde457eb42f406621d65a7d89354017acb
silkair <silkair1234@gmail.com>
2024-12-09 19:27:40 +0900

    fix(Application): backendApplication 구조 변경

--- src/main/java/com/sns255/sns255/global/Sns255Application.java
+++ src/main/java/com/sns255/sns255/global/Sns255Application.java
@@ -1,4 +1,4 @@
-package com.sns255.sns255;
+package com.sns255.sns255.global;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
