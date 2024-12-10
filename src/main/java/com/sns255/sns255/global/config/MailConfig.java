014909649726c8cfea4b63db2e33bd55980f70e7
silkair <silkair1234@gmail.com>
2024-12-11 05:48:57 +0900

    feat(config) : 이메일 전송 설정 구현

---
+++ src/main/java/com/sns255/sns255/global/config/MailConfig.java
@@ -0,0 +1,29 @@
+package com.sns255.sns255.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("silkair001@inu.com"); // 실제 이메일로 교체
+        mailSender.setPassword("ftud vqza dwkv hpub"); // 앱 비밀번호
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
