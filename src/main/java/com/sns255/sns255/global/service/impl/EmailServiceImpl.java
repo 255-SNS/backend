8e2f821cebec047f033d45972c00cb70b34e3824
silkair <silkair1234@gmail.com>
2024-12-11 05:52:13 +0900

    feat(service): 이메일 서비스 impl 구현

---
+++ src/main/java/com/sns255/sns255/global/service/impl/EmailServiceImpl.java
@@ -0,0 +1,33 @@
+package com.sns255.sns255.global.service.impl;
+
+import com.sns255.sns255.global.service.EmailService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+
+    @Override
+    public String generateVerificationToken() {
+        return UUID.randomUUID().toString(); // 랜덤 UUID 생성
+    }
+}
