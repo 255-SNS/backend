4088f66290666dcfd2def3cfc550d50404f8ad7a
silkair <silkair1234@gmail.com>
2024-12-12 17:54:16 +0900

    feat(repository): 이메일 레포지토리 구현

--- src/main/java/com/sns255/sns255/domain/email/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/sns255/sns255/domain/email/repository/EmailVerificationTokenRepository.java
@@ -1,4 +1,10 @@
 package com.sns255.sns255.domain.email.repository;
 
-public class EmailVerificationTokenRepository {
+import com.sns255.sns255.domain.email.entity.EmailVerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
+    Optional<EmailVerificationToken> findByToken(String token);
 }
