0710c46e33dd10af42d8c2e1d3ab969a33321e93
silkair <silkair1234@gmail.com>
2024-12-13 21:51:37 +0900

    fix(dto) : 로그인 요청 문구 수정 및 테스트

--- src/main/java/com/sns255/sns255/domain/user/dto/request/SignUpRequestDto.java
+++ src/main/java/com/sns255/sns255/domain/user/dto/request/SignUpRequestDto.java
@@ -22,7 +22,7 @@ public record SignUpRequestDto(
         String anonymousName,
 
         @NotBlank(message = "이메일은 필수 입력 사항입니다.")
-        @Schema(description = "회원의 이메일 주소", example = "user@example.com")
+        @Schema(description = "회원의 이메일 주소", example = "user@inu.ac.kr")
         @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message = "이메일 형식이 올바르지 않습니다.")
         String email,
 
